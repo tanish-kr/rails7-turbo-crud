4e7729c68e537721e6671cf85d6e6a6ccf24eec8
tanish-kr <tora.1986.tatsu@gmail.com>
2023-08-26 09:33:08 +0900

    add turbo chat rooms

---
+++ app/controllers/turbo_rooms_controller.rb
@@ -0,0 +1,25 @@
+class TurboRoomsController < ApplicationController
+  def index
+    @messages = TurboMessage.order(created_at: :desc).limit(20)
+  end
+
+  def new
+    @message = TurboMessage.new
+  end
+
+  def create
+    @message = TurboMessage.new(message_params)
+
+    if @message.save
+      flash.now.notice = "Message was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:turbo_message).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => "/cable"
   resources :posts
   resources :rooms
+  resources :turbo_rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
